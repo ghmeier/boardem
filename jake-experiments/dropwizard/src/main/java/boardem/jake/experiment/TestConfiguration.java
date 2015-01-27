f65874ae02f5ebb24d63421ea5b1d2856ac0a3eb
jakelong <jakelong@iastate.edu>
2015-01-27 15:08:49 -0600

    Added dropwizard experiment project

---
+++ jake-experiments/dropwizard/src/main/java/boardem/jake/experiment/TestConfiguration.java
@@ -0,0 +1,8 @@
+package boardem.jake.experiment;
+
+import com.yammer.dropwizard.config.*;
+
+public class TestConfiguration extends Configuration
+{
+	
+}
---
+++ jake-experiments/dropwizard/src/main/java/boardem/jake/experiment/TestService.java
@@ -0,0 +1,10 @@
+package boardem.jake.experiment;
+
+import com.yammer.dropwizard.Service;
+import com.yammer.dropwizard.config.Bootstrap;
+import com.yammer.dropwizard.config.Environment;
+
+public class TestService extends Service<TestConfiguration>
+{
+	
+}
