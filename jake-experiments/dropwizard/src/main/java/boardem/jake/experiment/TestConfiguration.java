dfea0e6a63388737983a4f43f5881750e9995a83
jakelong <jakelong@iastate.edu>
2015-01-27 15:49:26 -0600

    Added configuration files for dropwizard

--- jake-experiments/dropwizard/src/main/java/boardem/jake/experiment/TestConfiguration.java
+++ jake-experiments/dropwizard/src/main/java/boardem/jake/experiment/TestConfiguration.java
@@ -1,8 +1,42 @@
 package boardem.jake.experiment;
 
-import com.yammer.dropwizard.config.*;
+import org.hibernate.validator.constraints.NotEmpty;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.yammer.dropwizard.config.Configuration;
+
+/*
+ * Configuration class specifies environment-specific parameters
+ */
 public class TestConfiguration extends Configuration
 {
-	
+    @NotEmpty
+    private String template;
+
+    @NotEmpty
+    private String defaultName = "Stranger";
+
+    @JsonProperty
+    public String getTemplate() 
+    {
+        return template;
+    }
+
+    @JsonProperty
+    public void setTemplate(String template) 
+    {
+        this.template = template;
+    }
+
+    @JsonProperty
+    public String getDefaultName() 
+    {
+        return defaultName;
+    }
+
+    @JsonProperty
+    public void setDefaultName(String name) 
+    {
+        this.defaultName = name;
+    }
 }
