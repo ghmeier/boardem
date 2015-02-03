0b72aa3f0a1d99d06132b38c13ed7d351eef9b64
jakelong <jakelong@iastate.edu>
2015-02-02 21:27:38 -0600

    Added skeleton application for server. Put dropwizard dependency and other configurations in pom.xml

---
+++ boardem-server/src/main/java/boardem/server/BoardemConfiguration.java
@@ -0,0 +1,10 @@
+package bordem.server;
+
+import io.dropwizard.Configuration;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.hibernate.validator.constraints.NotEmpty;
+
+public class BoardemConfiguration extends Configuration
+{
+	
+}
