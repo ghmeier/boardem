11efc4175a58a19f54afa4698c76e332c89c5079
jakelong <jakelong@iastate.edu>
2015-02-12 18:37:37 -0600

    Re-added BoardemResponse.java

---
+++ boardem-server/src/main/java/boardem/server/json/BoardemResponse.java
@@ -0,0 +1,41 @@
+package boardem.server.json;
+
+/**
+ * Represents a JSON response object
+ */
+public class BoardemResponse
+{
+	private int code;
+	private String message;
+	
+	public BoardemResponse()
+	{
+		
+	}
+	
+	public BoardemResponse(int code, String message)
+	{
+		this.code = code;
+		this.message = message;
+	}
+	
+	public void setCode(int code)
+	{
+		this.code = code;
+	}
+	
+	public int getCode()
+	{
+		return code;
+	}
+	
+	public void setMessage(String message)
+	{
+		this.message = message;
+	}
+	
+	public String getMessage()
+	{
+		return message;
+	}
+}
