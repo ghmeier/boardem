ffb9ae51e0cc4033192649425dfcf04d7b0c49a4
be1 <be1@iastate.edu>
2015-02-14 20:17:09 -0600

    Added a method for auth via User object

--- boardem-server/src/main/java/boardem/server/logic/SignInLogic.java
+++ boardem-server/src/main/java/boardem/server/logic/SignInLogic.java
@@ -61,12 +61,46 @@ public class SignInLogic
 	}
 
 	/**
-	 * Logs a user in using a username and password
+	 * Logs a user in using a username and password (Must pass in "User" object)
 	 * @return BoardemResponse indicating if the operation completed succesfully, or the reason it failed
 	 */
 	public static BoardemResponse signIn(User user)
 	{
 		BoardemResponse response = null;
+		Firebase rootRef = new Firebase("https://boardem.firebaseio.com/");
+		
+		Firebase usersRef = rootRef.child("users");
+		Firebase facebookIdRef = rootRef.child("facebook_id");
+
+		DataSnapshot userData = FirebaseHelper.readData(usersRef);
+		
+		//Get the map of user data out of the data snapshot
+		//stringValues holds the JSON string before it is converted to a Java object
+		@SuppressWarnings({ "unchecked", "rawtypes" })
+		Map<String, HashMap> dataMap = (Map<String, HashMap>) userData.getValue();
+
+		if(dataMap == null)
+		{
+			//No users are in the database, so need to sign up
+
+			response = ResponseList.RESPONSE_USER_DOES_NOT_EXIST;
+		}
+		else
+		{
+			Map<String, User> users = FirebaseHelper.convertToObjectMap(dataMap, User.class);
+			
+			//Check if the username is already used
+			if(users.containsKey(user.getFacebookId()))
+			{
+				//If already used, success!
+				response = ResponseList.RESPONSE_SUCCESS;
+			}
+			else
+			{
+
+				response = ResponseList.RESPONSE_USER_DOES_NOT_EXIST;
+			}
+		}
 
 		return response;
 	}
