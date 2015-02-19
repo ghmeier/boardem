c8bd7771459ed41a5e23be302df8b5015eb1b2f2
lgeiken <lgeiken@iastate.edu>
2015-02-19 00:10:25 -0600

    Moved app stuctures to angular services for a more structured application base

--- boardem-app/boardem/www/js/controllers.js
+++ boardem-app/boardem/www/js/controllers.js
@@ -1,12 +1,55 @@
 var appCtrl =angular.module('starter.controllers', ['ionic','firebase'])
 
-.controller('MenuCtrl', function($rootScope, $scope, $ionicHistory) {
+.controller('MenuCtrl', function($rootScope, $scope, $ionicHistory,SearchCriteria) {
+	
+	var dateObject = new Date();
+	$scope.daysArray = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+	$scope.monthsArray = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+	//Get date in numbers
+  $scope.month = dateObject.getMonth();
+	$scope.day = dateObject.getDate();
+	$scope.year = dateObject.getFullYear();
+	//Get date in string format
+	$scope.monthString = $rootScope.monthsArray[dateObject.getMonth()];
+	$scope.dayString = $rootScope.daysArray[dateObject.getDay()];
+	
+	$scope.locationSearch = "1";
+	$scope.gameType = "all";
+
 	$ionicHistory.clearHistory();
 	 $scope.changeDate = function(direction) {
 			$rootScope.changeDate(direction);
   };
+	
+	//Actions
+	$scope.changeDate = function(direction){
+			SearchCriteria.changeDateSearch(direction);
+	}
 
 });
+
+.service('SearchCriteria', ['$http', function ($scope, $http) {
+	
+		this.changeDateSearch = function (direction) {
+					
+				var dateObject = new Date($scope.year, $scope.month, $scope.day);
+				if(direction == 1 || direction == "1") {
+					dateObject.setDate(dateObject.getDate()+1);
+				} else {
+					dateObject.setDate(dateObject.getDate()-1);
+				}
+			
+				//Get date in numbers
+				$scope.month = dateObject.getMonth();
+				$scope.day = dateObject.getDate();
+				$scope.year = dateObject.getFullYear();
+				//Get date in string format
+				$scope.monthString = $scope.monthsArray[dateObject.getMonth()];
+				$scope.dayString = $scope.daysArray[dateObject.getDay()];
+				//It will look like this
+				//return $http.get(urlBase);
+		};
+}]);
 
 function safeApply($scope, $root, fn) {
   var phase = $root.$$phase;
--- boardem-app/boardem/www/js/controllers/eventsCtrl.js
+++ boardem-app/boardem/www/js/controllers/eventsCtrl.js
@@ -1,8 +1,21 @@
-appCtrl.controller('eventsCtrl', function($rootScope, $scope, $http) {
+appCtrl.controller('eventsCtrl', function($rootScope, $scope, $http, RestFactory) {
 
-  $scope.events = [
-    { title: 'John\'s game night.', id: 1 },
-    { title: 'Mandy has some games', id: 2 },
-  ];
+  $scope.events = RestFactory.getEvents();
   
 });
+
+appCtrl.service('RestFactory', ['$http', function ($rootScope, $http) {
+	
+        var urlBase = '/events';
+				
+				//Any other http request will be used as a service function
+        this.getEvents = function () {
+					
+					return [
+							{ title: 'John\'s game night.', id: 1 },
+							{ title: 'Mandy has some games', id: 2 },
+					];
+						//It will look like this
+            //return $http.get(urlBase);
+        };
+}]);
