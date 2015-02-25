4fd4d850ddf194fc21fd75c210a0bb5e1310417c
lgeiken <lgeiken@iastate.edu>
2015-02-25 00:15:52 -0600

    Working on services usage

--- boardem-app/boardem/www/js/controllers/eventsCtrl.js
+++ boardem-app/boardem/www/js/controllers/eventsCtrl.js
@@ -3,37 +3,12 @@ appCtrl.controller('eventsCtrl', function($rootScope, $scope, $http, $ionicPopup
 	$scope.day = SearchCriteria.getDay();
 	$scope.month = SearchCriteria.getMonth();
 	$scope.year = SearchCriteria.getYear();
-
+	
 	$scope.loadEvents = function(){
-		$scope.events = []
-		RestService.getEvents($rootScope.SERVER_LOCATION).success(function(res){
-			$scope.event_ids = res.extra;
-			for (id in $scope.event_ids){
-				var id_string = $scope.event_ids[id];
-				EventService.getEvent($rootScope.SERVER_LOCATION,$scope.event_ids[id]).success(function(res){
-					safeApply($scope,$rootScope,function(){
-
-						$scope.events.push(res.extra);
-						var num = $scope.events.length-1;
-						$scope.events[num].time = EventService.getTimeDifference(res.extra.date);
-						$scope.events[num].canJoin = EventService.isParticipant($rootScope.user_id,$scope.events[num]);
-						$scope.events[num].isOwner = EventService.isOwner($rootScope.user_id,$scope.events[num]);
-
-						UserService.getUser($rootScope.SERVER_LOCATION,$scope.events[num].owner).success(function(response){
-							$scope.events[num].owner_profile = response.extra;
-						});
-					});
-				});
-			}
-		}).error(function(error){
-			$ionicPopup.alert({
-				title: "Failed to Retrieve events.",
-				template: "Error: "+error
-			});
-		});
+		$scope.events = EventService.loadEvents();
 	};
 
-/*	$ionicPlatform.ready(function(){
+/*$ionicPlatform.ready(function(){
 		$scope.loadEvents();
 	});
 */
@@ -79,14 +54,3 @@ appCtrl.controller('eventsCtrl', function($rootScope, $scope, $http, $ionicPopup
 	};
   
 });
-
-appCtrl.service('RestService', ['$http', function ($http) {
-	
-        var endpoint = 'event';
-
-        this.getEvents = function (base_url) {
-
-			return $http.get(base_url+endpoint);
-
-        };
-}]);
--- boardem-app/boardem/www/js/controllers/profileCtrl.js
+++ boardem-app/boardem/www/js/controllers/profileCtrl.js
@@ -20,11 +20,3 @@ appCtrl.controller('profileCtrl',function($rootScope,$ionicPopup,$http, $scope, 
 	    });
 
 });
-
-appCtrl.service("UserService",['$http',function($http){
-
-	var endpoint = "users/"
-	this.getUser = function(base_url,userid){
-		return $http.get(base_url+endpoint+userid);
-	}
-}]);
