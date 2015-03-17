e70be40040ea4a6d9c9a5a649df248a9dcbde664
be1 <be1@iastate.edu>
2015-03-16 23:26:38 -0500

    Comments and optimization fixes

--- boardem-server/src/main/java/boardem/server/logic/UserBadgesLogic.java
+++ boardem-server/src/main/java/boardem/server/logic/UserBadgesLogic.java
@@ -18,57 +18,66 @@ public class UserBadgesLogic {
 	{
 		BoardemResponse response = null;
 		
+		//Point badgesRef to the "badges" table of the user
 		Firebase rootRef = new Firebase(BoardemApplication.FIREBASE_URL);
 		Firebase badgesRef = rootRef.child("users").child(UserLogic.getStringNameFromId(uid)).child("badges");
 
+		//Create ArrayList used in line 42
 		ArrayList<String> badgeIds = new ArrayList<String>();
 		
+		//Convert badgesRef into a DataSnapshot
 		DataSnapshot badgesData = FirebaseHelper.readData(badgesRef);
-		
-		if (badgesData == null) {
-			return ResponseList.RESPONSE_NO_BADGES;
-		}
 
 		@SuppressWarnings({ "rawtypes", "unchecked" })
+		//Convert badgeIdMap into a HashMap
 		Map<String, HashMap> badgeIdMap = (Map<String, HashMap>) badgesData.getValue();
 
-		if (badgeIdMap != null) {
+		//Check if user has any badges
+		if (badgeIdMap == null) {
+
+			return ResponseList.RESPONSE_NO_BADGES;
+		//If they do have badges...
+		} else {
+			//Create a new HashMap with the data inside it
 			Map<String, Object> realBadgeIdMap = FirebaseHelper.convertToObjectMap(badgeIdMap, Object.class);
 			badgeIds.addAll(realBadgeIdMap.keySet());
 		}
 
-
+		//Return success
 		response = ResponseList.RESPONSE_SUCCESS;
 
-		//change once everything works
+		//Add badge IDs to the response
 		response.setExtra(badgeIds);
+
 		return response;
 		
 	}
 
 	public static BoardemResponse addUserBadge(String user_id, String badge_id)
 	{
-		BoardemResponse response = null;
-
+		//Put username getting in front so we don't have to potentially do it twice
 		String username = UserLogic.getStringNameFromId(user_id);
 
+		//Point badgesRef to the "badges" table of username "user_id"
 		Firebase rootRef = new Firebase(BoardemApplication.FIREBASE_URL);
 		Firebase badgesRef = rootRef.child("users").child(username).child("badges");
 
+		//Convert badgesRef to a DataSnapshot
 		DataSnapshot badgesData = FirebaseHelper.readData(badgesRef);
 
 		@SuppressWarnings({"rawtypes", "unchecked"})
+		//Convert badgesData to a HashMap
 		Map<String,Object> badgesMap = (Map<String,Object>) badgesData.getValue();
 
+		//If the user doesn't have any badges
 		if (badgesMap == null) {
-			//Firebase initialRef = rootRef.child("users").child(username);
-
-			//badgesData = FirebaseHelper.readData(initialRef);
 
 			Map<String,String> initialVal = new HashMap<String,String>();
 
+			//Add the first value
 			initialVal.put(badge_id, "0");
 
+			//Write it to the Firebase in the "badges" table
 			FirebaseHelper.writeData(badgesRef, initialVal);
 
 		} else {
@@ -76,44 +85,44 @@ public class UserBadgesLogic {
 
 			Map<String,String> contactMap = new HashMap<String,String>();
 
+			//To count the number of badges the user has already
 			int size = badgesMap.size();
 
 			contactMap.put(badge_id, Integer.toString(size));
 
+			//Write the HashMap to Firebase under the "badges" table
 			FirebaseHelper.writeData(badgesRef, contactMap);
 
 		}
 
-		response = ResponseList.RESPONSE_SUCCESS;
-
-		return response;
+		return ResponseList.RESPONSE_SUCCESS;
 	}
 	
 	@SuppressWarnings("unchecked")
+	// ^^^ Necessary anymore? I'm not using Eclipse
 	public static BoardemResponse removeUserBadge(String user_id, String badge_id)
 	{
-		BoardemResponse response = null;
+		//Point badgesRef to the badge_id of user "user_id"
+		Firebase badgesRef = new Firebase(BoardemApplication.FIREBASE_URL).child("users").child(UserLogic.getStringNameFromId(user_id)).child("badges").child(badge_id);
 
-		String username = UserLogic.getStringNameFromId(user_id);
-
-		Firebase badgesRef = new Firebase(BoardemApplication.FIREBASE_URL).child("users").child(username).child("badges").child(badge_id);
-
+		//Convert to DataSnapshot
 		DataSnapshot badgesData = FirebaseHelper.readData(badgesRef);
 
 		@SuppressWarnings({"rawtypes", "unchecked"})
+		//Convert to string (will only have one value, so no HashMap this time)
 		String badgesExists = (String) badgesData.getValue();
 
+		//If badgesExists = null, no badge with the given badge_id exists in that user's list of badges
 		if (badgesExists == null) {
 
 			return ResponseList.RESPONSE_BADGE_DOES_NOT_EXIST;
 
 		} 
 
+		//If the badge_id does exist in the user, remove it
 		FirebaseHelper.removeData(badgesRef);
-	
-		response = ResponseList.RESPONSE_SUCCESS;
 
-		return response;
+		return ResponseList.RESPONSE_SUCCESS;
 	}
 
 }
