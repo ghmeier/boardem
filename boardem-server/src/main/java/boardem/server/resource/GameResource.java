897bd0aab554a58e1f8775db4ebcef36cde804a6
jakelong <jakelong@iastate.edu>
2015-03-22 13:14:57 -0500

    Moved /game to /games

--- boardem-server/src/main/java/boardem/server/resource/GameResource.java
+++ boardem-server/src/main/java/boardem/server/resource/GameResource.java
@@ -12,7 +12,7 @@ import javax.ws.rs.core.Response;
 import boardem.server.logic.GetGameLogic;
 import boardem.server.logic.GetGamesLogic;
 
-@Path("/game")
+@Path("/games")
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
 public class GameResource
