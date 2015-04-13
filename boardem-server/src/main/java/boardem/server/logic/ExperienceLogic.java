b9a3ae1bb9ad38297300eaedb526cdb7894fb43f
be1 <be1@iastate.edu>
2015-04-12 20:55:09 -0500

    Fixed "exp" to give users experience on badge completion too.

--- boardem-server/src/main/java/boardem/server/logic/ExperienceLogic.java
+++ boardem-server/src/main/java/boardem/server/logic/ExperienceLogic.java
@@ -2,13 +2,19 @@ package boardem.server.logic;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Map;	
+import java.util.Map;
+import java.util.List;
+
+import java.lang.Math;	
 
 import boardem.server.BoardemApplication;
 import boardem.server.FirebaseHelper;
 import boardem.server.json.BoardemResponse;
 import boardem.server.json.ResponseList;
 import boardem.server.logic.UserLogic;
+import boardem.server.BadgeActions;
+import boardem.server.json.Badge;
+import boardem.server.logic.BadgeLogic;	
 
 import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
@@ -56,17 +62,30 @@ public class ExperienceLogic {
  	*/
 	public static BoardemResponse setUserExperience(String user_id, Long exp)
 	{
+		Firebase rootRef = new Firebase(BoardemApplication.FIREBASE_URL);
+
 		if (exp == null) {
 			return ResponseList.RESPONSE_NULL_INPUT;
 		}
 
 		String userName = UserLogic.getStringNameFromId(user_id);
 
+		Firebase levelRef = rootRef.child("users").child(userName).child("level");
+
+		DataSnapshot levelSnap = FirebaseHelper.readData(levelRef);
+
+		long prevLevel = 0;
+
+		if (levelSnap != null && levelSnap.getValue() != null) {
+			prevLevel = (long) levelSnap.getValue();
+		}
 
 		long experience;
 
+		long level;
+
 		//Point experienceRef to the "experience" table of the user
-		Firebase rootRef = new Firebase(BoardemApplication.FIREBASE_URL);
+
 		Firebase experienceRef = rootRef.child("users").child(userName).child("experience");
 
 		DataSnapshot experienceSnap = FirebaseHelper.readData(experienceRef);
@@ -79,11 +98,17 @@ public class ExperienceLogic {
 
 			Map<String,Long> initialVal = new HashMap<String,Long>();
 
+			//Gives us an integer value for level based on D&D 3.5 leveling rules
+			//Magic. Pure magic.
+			level = (long) Math.floor((Math.sqrt(5*((double)exp+125))+25)/50);
+
 			//Add the first value
 			initialVal.put("experience", exp);
+			initialVal.put("level", level);
 
 			//Write it to the Firebase in the "experience" table
 			FirebaseHelper.writeData(newExpRef, initialVal);
+
 
 		//Otherwise, if the user already has a table, add the current value to what already exists
 		} else {
@@ -98,15 +123,30 @@ public class ExperienceLogic {
 
 			Map<String,Long> updatedVal = new HashMap<String,Long>();
 
+			//Magic happens
+			level = (long) Math.floor((Math.sqrt(5*((double)experience+125))+25)/50);			
+
 			//Update the value 
 			updatedVal.put("experience", experience);
+			updatedVal.put("level", level);
 
 			//Write it to the Firebase in the "experience" table
 			FirebaseHelper.writeData(newExpRef, updatedVal);
 
 		}
 
-		return ResponseList.RESPONSE_SUCCESS;
+		BoardemResponse response = ResponseList.RESPONSE_SUCCESS.clone();
+
+		if (prevLevel != level) {
+		//Update the user's badge progress
+			List<Badge> earnedBadges = BadgeLogic.updateBadge(user_id, BadgeActions.ACTION_LEVEL);
+			for(Badge b : earnedBadges)
+			{
+				response.addBadge(b);
+			}
+		}
+
+		return response;
 	}
 
 }
